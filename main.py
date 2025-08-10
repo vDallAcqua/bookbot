320e06d1c25a2ac0205108b7e02a797f8c0e467d
vDallAcqua <vdallacqua@gmail.com>
2025-08-11 00:29:19 +0200

    Printing report

--- main.py
+++ main.py
@@ -1,15 +1,34 @@
 from stats import get_num_words
+from stats import get_num_letters
+from stats import sort_letters_by_count
 
 def get_book_text(path_to_file):
     with open(path_to_file) as f:
         # f is a file object
         return f.read()
-        
+
+def print_report(file_name, words, ord_letters):
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
+    
+    print("----------- Word Count ----------")    
+    print(f"Found {words} total words")
+
+    print("--------- Character Count -------")
+    for item in ord_letters:
+        if (item[key]).isalpha():
+            print(f"{item[key]}: {item[value]}")
 
 def main():
-    file_contents = get_book_text('./books/frankenstein.txt')
+    file_name = 'books/frankenstein.txt'
+    file_contents = get_book_text(file_name)
     # #1 objective: print(file_contents)
-    words = count_words(file_contents)
-    print(f"{words} words found in the document")
+    
+    words = get_num_words(file_contents)
+    
+    num_letters = get_num_letters(file_contents)
+    ord_letters = sort_letters_by_count(num_letters)
+    print_report(file_name, words, ord_letters) 
+    
 
 main()
--- stats.py
+++ stats.py
@@ -1,3 +1,17 @@
-def count_words(content):
+def get_num_words(content):
     content_words = content.split()
     return len(content_words)
+
+def get_num_letters(content):
+    letter_count = {}
+    lower_content = content.lower()
+    for char in lower_content:
+        if char in letter_count:
+            letter_count[char] += 1
+        else:
+            letter_count[char] = 1
+    return letter_count
+
+def sort_letters_by_count(num_letters):
+    sorted_occurrences = sorted(num_letters.items(), key=lambda item: item[1], reverse=True)
+    return sorted_occurrences
